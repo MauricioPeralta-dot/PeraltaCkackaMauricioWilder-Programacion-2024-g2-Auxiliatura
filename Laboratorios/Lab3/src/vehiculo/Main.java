b1822d1a6a537b7f77ce1401649e6e91ebffd00b
MauricioPeralta-dot <peraltamauricio78@gmail.com>
2024-10-05 20:37:36 -0400

    Laboratorio 3

---
+++ Laboratorios/Lab3/src/vehiculo/Main.java
@@ -0,0 +1,11 @@
+package vehiculo;
+
+public class Main {
+    public static void main(String[] args) {
+        ElectricVehicle myEV = new ElectricVehicle("Tesla", "Model 3", 50000, 75, 500);
+        myEV.start();
+        myEV.displayInfo();
+        myEV.displayRange();
+        myEV.stop();
+    }
+}
